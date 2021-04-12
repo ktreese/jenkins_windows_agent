12fe47dd4b8b8f12fe358b69f8c3780897f58ca8
akom <alexander.komarov@softwareag.com>
2021-04-12 11:46:23 -0400

    Add swarm options, correct labels quoting

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,25 +2,27 @@
 #
 #
 class jenkins_windows_agent::params {
-  $client_source            = 'repo.jenkins-ci.org'
-  $version                  = '3.3'
-  $verify_peer              = false
-  $swarm_mode               = 'exclusive'
-  $swarm_executors          = '8'
-  $swarm_labels             = 'windows'
-  $disable_ssl_verification = '-disableSslVerification'
-  $agent_drive              = 'C:'
-  $agent_home               = '/opt/ci/jenkins/'
-  $jenkins_dirs             = ['/opt/ci/jenkins/', '/opt/ci/jenkins/workspace/', '/tmp']
-  $jenkins_master_url       = 'http://myjenkinsmaster.localhost:8080'
-  $jenkins_master_user      = 'jenkins'
-  $jenkins_master_pass      = 'pass123'
-  $service_name             = 'Jenkins_Agent'
-  $service_user             = 'LocalSystem'
-  $service_pass             = undef
-  $service_interactive      = false
-  $create_user              = false
-  $jdk                      = 'jdk8'
-  $jdk_choco_version        = 'latest'
-  $java                     = 'java.exe'
+  $client_source             = 'repo.jenkins-ci.org'
+  $version                   = '3.3'
+  $verify_peer               = false
+  $swarm_mode                = 'exclusive'
+  $swarm_executors           = '8'
+  $swarm_labels              = 'windows'
+  $disable_clients_unique_id = false
+  $disable_ssl_verification  = true
+  $swarm_client_args         = ''
+  $agent_drive               = 'C:'
+  $agent_home                = '/opt/ci/jenkins/'
+  $jenkins_dirs              = ['/opt/ci/jenkins/', '/opt/ci/jenkins/workspace/', '/tmp']
+  $jenkins_master_url        = 'http://myjenkinsmaster.localhost:8080'
+  $jenkins_master_user       = 'jenkins'
+  $jenkins_master_pass       = 'pass123'
+  $service_name              = 'Jenkins_Agent'
+  $service_user              = 'LocalSystem'
+  $service_pass              = undef
+  $service_interactive       = false
+  $create_user               = false
+  $jdk                       = 'jdk8'
+  $jdk_choco_version         = 'latest'
+  $java                      = 'java.exe'
 }
